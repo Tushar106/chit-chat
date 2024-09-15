508e6c921064d0b6a03dd8898058ac6bdd3eba57
Tushar106 <tusharbhatia106@gmail.com>
2024-09-15 17:51:40 +0530

    added the server to render

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-function SignIn(){
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false)
     const [toast, setToast] = useState(false)
-    const [error,setError]=useState("")
-    const navigate=useNavigate()
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
     function empty() {
         if (email === "")
             return "Please Enter your email"
         else if (password === "")
             return "Please Enter your password"
-        else if(error!=="")
-        return error
+        else if (error !== "")
+            return error
     }
     function Toast(props) {
         return (
@@ -35,23 +35,23 @@ function SignIn(){
     }
 
     const submit = async () => {
-        if (email === "" || password === "" ) {
+        if (email === "" || password === "") {
             setToast(true)
         }
         else {
-            const body={
-                email:email,
-                password:password,
+            const body = {
+                email: email,
+                password: password,
             }
             try {
                 setLoading(true)
-                const response=await axios.post("https://chit-chat-server-7y4zp2xxs-tushar106s-projects.vercel.app/api/user/login",body,{
-                    headers:{
+                const response = await axios.post("/api/user/login", body, {
+                    headers: {
                         Accept: "application/json",
                         "Content-Type": "application/json"
                     }
                 })
-                localStorage.setItem("user",JSON.stringify(response.data))
+                localStorage.setItem("user", JSON.stringify(response.data))
                 setLoading(false)
                 navigate("/chat")
             } catch (error) {
@@ -62,26 +62,26 @@ function SignIn(){
             }
         }
     }
-    return(
-<form>
-                            <div className="form-outline mb-4">
-                                <label className="form-label" htmlFor="form3Example3">Email address</label>
-                                <input type="email"  className="form-control form-control-lg"
-                                    placeholder="Enter a valid email address" onChange={(e)=>{setEmail(e.target.value)}} value={email}  />
-                            </div>
+    return (
+        <form>
+            <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="form3Example3">Email address</label>
+                <input type="email" className="form-control form-control-lg"
+                    placeholder="Enter a valid email address" onChange={(e) => { setEmail(e.target.value) }} value={email} />
+            </div>
 
-                            <div className="form-outline mb-3">
-                                <label className="form-label" htmlFor="form3Example4">Password</label>
-                                <input type="password"  className="form-control form-control-lg"
-                                    placeholder="Enter password" onChange={(e)=>{setPassword(e.target.value)}} value={password}  />
-                            </div>
-                            <div className="text-center text-lg-start mt-4 pt-2">
-                                <button type="button" className="btn btn-primary btn-lg"
-                                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }} onClick={submit} disabled={loading}>Login</button>
-                            </div>
-                            {toast && <Toast data={empty()} />}
+            <div className="form-outline mb-3">
+                <label className="form-label" htmlFor="form3Example4">Password</label>
+                <input type="password" className="form-control form-control-lg"
+                    placeholder="Enter password" onChange={(e) => { setPassword(e.target.value) }} value={password} />
+            </div>
+            <div className="text-center text-lg-start mt-4 pt-2">
+                <button type="button" className="btn btn-primary btn-lg"
+                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }} onClick={submit} disabled={loading}>Login</button>
+            </div>
+            {toast && <Toast data={empty()} />}
 
-                        </form>
+        </form>
     )
 }
 export default SignIn
