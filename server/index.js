178c627e5cbf842e8a069202a992957483d6d88e
Tushar106 <tusharbhatia106@gmail.com>
2024-09-15 14:26:21 +0530

    added the middleware for res

--- server/index.js
+++ server/index.js
@@ -49,6 +49,14 @@ app.get("/", (req, res) => {
     res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.send("Hello world")
 })
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    next();
+})
 
 
 app.use("/api/user", userRoute);
