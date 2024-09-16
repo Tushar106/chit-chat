275b22ad37e4f572811671133ce3a64763215a5c
Tushar106 <tusharbhatia106@gmail.com>
2024-09-16 21:58:20 +0530

    frontend

--- frontend/src/components/ChatList.js
+++ frontend/src/components/ChatList.js
@@ -41,7 +41,6 @@ function ChatList({ fetchChat, setfetchChat }) {
                 const { data } = await axios.get("https://chit-chat-server-7lyn.onrender.com/api/chat",{
                     withCredentials:true,
                     headers: {
-                        'Access-Control-Allow-Origin': '*',
                         Accept: "application/json",
                         "Content-Type": "application/json"
                     }
